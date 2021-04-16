e0f5b655c807517eaaad82a21e22d416fb17771c
Slav Donchev <slav.donchev@gmail.com>
2021-04-16 13:13:24 +0300

    add datasource to allow for apply delay

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
-variable "sleep_length" {
+variable "sleep_length_plan" {
   type    = number
   default = 60
 }
 
+variable "sleep_length_apply" {
+  type    = number
+  default = 60
+}
+
+data "external" "sleep" {
+  program = ["${path.module}/sleep.sh", var.sleep_length_plan]
+} 
+
 resource "null_resource" "sleep" {
   provisioner "local-exec" {
-    command = "sleep ${var.sleep_length}"
+    command = "sleep ${var.sleep_length_apply}"
   }
   triggers = {
     always = uuid()
